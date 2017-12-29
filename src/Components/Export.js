2c32847a87e22b4f7e9290dc87070f4de1623756
Wes Basinger <wbasinger@villagetechschools.org>
2017-12-29 14:43:03 -0600

    Added link on footer for exporting data.

--- src/Components/Export.js
+++ src/Components/Export.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { CSVLink, CSVDownload } from 'react-csv';
+import { CSVLink } from 'react-csv';
 
 class Export extends Component {
 
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 
+import { Link } from 'react-router-dom';
+
 class Footer extends Component {
   render() {
     return (
 		<div className="starter-template">
 			<p>App written and maintained by Wes Basinger.  Contact dbumathlete@gmail.com with any errors or feedback.</p>
+      <small><Link to="/export">Export All Data</Link></small>
 		</div>
 	)
   }
